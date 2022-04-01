c4c1c001811a75338c26bd5460c2f4f792bd7aac
boundedkebab <unalmahmut84@gmail.com>
2022-04-01 20:35:44 +0300

    Added remove joke one by one and some design

--- Joke-API/app.js
+++ Joke-API/app.js
@@ -1,31 +1,26 @@
-const jokeContainer = document.querySelector(".container")
-const box = document.querySelector(".box")
+const jokeContainer = document.querySelector(".container");
+const box = document.querySelector(".box");
 
-const initJoke = async()=> {
-    await getJoke()
-    
-}
+const initJoke = async () => {
+    await getJoke();
+};
 
-const getJoke= async()=>{
-    let url = `https://v2.jokeapi.dev/joke/Any`
-    let res = await fetch(url)
-    let data = await res.json()
-    if(data.type == "twopart") {
-        dataTwo(data)
+const getJoke = async () => {
+    let url = `https://v2.jokeapi.dev/joke/Any`;
+    let res = await fetch(url);
+    let data = await res.json();
+    if (data.type == "twopart") {
+        dataTwo(data);
+    } else {
+        dataSingle(data);
     }
-    else{
-        dataSingle(data)
-    }
+};
+const dataTwo = (joke) => {
+    const setup = joke.setup;
+    const delivery = joke.delivery;
 
-
-}
-const dataTwo = (joke)=>{
-
-    const setup = joke.setup
-    const delivery = joke.delivery
-
-    const jokeEl = document.createElement("div")
-    jokeEl.classList.add("box")
+    const jokeEl = document.createElement("div");
+    jokeEl.classList.add("box");
 
     jokeEl.innerHTML = `
     <h3 id="joke-category">Category</h3>
@@ -33,25 +28,15 @@ const dataTwo = (joke)=>{
     <p id="joke-delivery">${delivery}</p>
     <p id="safe">Safe</p>
     
-    `
-    jokeContainer.appendChild(jokeEl)
-
-
-sdssd
-}
-const dataSingle = (joke)=>{
-    console.log(joke.joke + "SINGLE")
-
-
-}
-const deleteJoke = ()=>{
-    jokeContainer.innerHTML = ""
-
-}
-    const deleteJoke1 = ()=>{
-       
-        jokeContainer.removeChild(jokeContainer.firstChild)
-        
-}
-
-
+    `;
+    jokeContainer.appendChild(jokeEl);
+};
+const dataSingle = (joke) => {
+    console.log(joke.joke + "SINGLE");
+};
+const deleteJoke = () => {
+    jokeContainer.innerHTML = "";
+};
+const deleteJoke1 = () => {
+    jokeContainer.removeChild(jokeContainer.firstChild);
+};
